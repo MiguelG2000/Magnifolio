db6ae213d2898ac9e461d16f3e7a73e5472fec3d
MiguelG2000 <mikegonzalez@MacBook-Air-de-Miguel.local>
2025-08-28 14:08:47 -0600

    Editar y eliminar proyecto

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import agregar_proyecto
+from .views import (
+    agregar_proyecto,
+    editar_proyecto,
+    eliminar_proyecto,
+)
 
 urlpatterns = [
     path('agregar/', agregar_proyecto, name='agregar_proyecto'),
+    path('editar/<int:proyecto_id>/', editar_proyecto, name='editar_proyecto'),
+    path('eliminar/<int:proyecto_id>/', eliminar_proyecto, name='eliminar_proyecto'),
 ]
--- proyectos/views.py
+++ proyectos/views.py
@@ -3,15 +3,16 @@ from lib2to3.fixes.fix_input import context
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.forms import modelformset_factory
+from django.contrib import messages
 from .forms import ProyectoForm, FotoProyectoForm
 from .models import Proyecto, FotoProyecto
 
 @login_required
 def agregar_proyecto(request):
-    FotoFormSet = modelformset_factory(FotoProyecto, form=FotoProyectoForm, extra=3)
+    FotoFormSet = modelformset_factory(FotoProyecto, form=FotoProyectoForm, extra=1)
 
     if request.method == 'POST':
         form = ProyectoForm(request.POST)
@@ -37,3 +38,50 @@ def agregar_proyecto(request):
         'formset': formset
     }
     return render(request, 'agregar_proyecto.html', context)
+
+@login_required
+def editar_proyecto(request, proyecto_id):
+    proyecto = get_object_or_404(Proyecto, proyecto_id=proyecto_id, usuario=request.user)
+    FotoFormSet = modelformset_factory(FotoProyecto, form=FotoProyectoForm, extra=1, can_delete=True)
+
+    if request.method == 'POST':
+        form = ProyectoForm(request.POST, instance=proyecto)
+        formset = FotoFormSet(request.POST, request.FILES, queryset=FotoProyecto.objects.filter(proyecto=proyecto))
+
+        if form.is_valid() and formset.is_valid():
+            form.save()
+
+            for foto_form in formset:
+                if foto_form.cleaned_data.get('DELETE'):
+                    if foto_form.instance.pk:
+                        foto_form.instance.delete()
+                elif foto_form.cleaned_data.get('imagen'):
+                    foto_form.save(commit=False)
+                    foto_form.instance.proyecto = proyecto
+                    foto_form.save()
+
+            return redirect('mi_portafolio')
+    else:
+        form = ProyectoForm(instance=proyecto)
+        formset = FotoFormSet(queryset=FotoProyecto.objects.filter(proyecto=proyecto))
+
+    context = {
+        'form': form,
+        'formset': formset
+    }
+    return render(request, 'agregar_proyecto.html', context)
+
+@login_required
+def eliminar_proyecto(request, proyecto_id):
+    proyecto = get_object_or_404(Proyecto, proyecto_id=proyecto_id, usuario=request.user)
+
+    if request.method == 'POST':
+        proyecto.delete()
+        messages.success(request, "Proyecto eliminado correctamente.")
+        return redirect('mi_portafolio')
+
+    context = {
+        'proyecto': proyecto
+    }
+
+    return render(request, 'eliminar_proyecto.html', context)
